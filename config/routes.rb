e86c187fcae3e83152edf024458473ec4550d6a3
Matt-Novich <mnovich@chicagobooth.edu>
2021-05-17 18:54:07 +0000

    Index and show pages drafted

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get("/users", {:controller => "user", :action => "index"})
   get("/users/:path_id", {:controller => "user", :action => "show"})
 
+  get("/photos", {:controller => "photo", :action => "index"})
+  get("/photos/:path_id", {:controller => "photo", :action => "show"})
+
   get("/update")
 end
